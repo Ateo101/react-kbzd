474baf0f1c4bac81c52f043018d1f433ad560097
Ateo101 <nikmail242@gmail.com>
2022-11-27 19:58:17 +0100

    Rating component improvement

--- src/components/Rating/UncontrolledRating.tsx
+++ src/components/Rating/UncontrolledRating.tsx
@@ -5,7 +5,7 @@ import s from '../../App.module.css'
 const UncontrolledRating = () => {
 
     const [value, setValue] = useState(0)
-    const values = [1, 2, 3, 4, 5]
+    const possibleRating = [1, 2, 3, 4, 5]
 
     const onClickHandler = (v: number) => () => {
         setValue(v)
@@ -16,17 +16,9 @@ const UncontrolledRating = () => {
 
     return (
         <div className={s.starContainer}>
-
-            {/*<Star selected={value > 0} onClickCallback={onClickHandler(1)} onDoubleClickCallback={onDoubleClickHandler}/>
-            <Star selected={value > 1} onClickCallback={onClickHandler(2)} onDoubleClickCallback={onDoubleClickHandler}/>
-            <Star selected={value > 2} onClickCallback={onClickHandler(3)} onDoubleClickCallback={onDoubleClickHandler}/>
-            <Star selected={value > 3} onClickCallback={onClickHandler(4)} onDoubleClickCallback={onDoubleClickHandler}/>
-            <Star selected={value > 4} onClickCallback={onClickHandler(5)} onDoubleClickCallback={onDoubleClickHandler}/>*/}
-
-            {values.map( (v) => <Star selected={value > v-1}
-                                              onClickCallback={onClickHandler(v)}
-                                              onDoubleClickCallback={onDoubleClickHandler}/> )}
-
+            {possibleRating.map((v) => <Star selected={value > v - 1}
+                                             onClickCallback={onClickHandler(v)}
+                                             onDoubleClickCallback={onDoubleClickHandler}/>)}
         </div>
     )
 
